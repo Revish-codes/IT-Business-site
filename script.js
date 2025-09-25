87ff51a42d4039c9db5611a47586441f21bf80ee
Revish-codes <dsouzarevish41@gmail.com>
2025-09-25 15:25:41 +0530

    index changes

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+  document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".phone-menu h1");
+    const menu = document.querySelector(".phone-menu ul");
+    const closeBtn = document.querySelector(".phone-menu .close-btn");
+
+    hamburger.addEventListener("click", () => {
+      menu.classList.add("active"); // open
+    });
+
+    closeBtn.addEventListener("click", () => {
+      menu.classList.remove("active"); // close
+    });
+  });
+
 const faqs = document.querySelectorAll(".faq-item");
 
 faqs.forEach(faq => {
